d8fab0b311466f7bf11a579e4e50f63915a918ae
Steven <steven.decock@realdolmen.com>
2019-05-23 23:50:28 +0200

    added test method

--- src/HelloWold.java
+++ src/HelloWold.java
@@ -4,4 +4,8 @@ public class HelloWold {
 	public static void main(String[] args) {
 		System.out.println("mlskjdmfkld");
 	}
+
+	public static void test(){
+		System.out.println("git hello");
+	}
 }
