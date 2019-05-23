2ad308935db880e30f84f6f61fa64181c3956aa1
Steven <steven.decock@realdolmen.com>
2019-05-23 23:49:48 +0200

    test method

--- src/HelloWold.java
+++ src/HelloWold.java
@@ -3,5 +3,10 @@ public class HelloWold {
 	
 	public static void main(String[] args) {
 		System.out.println("mlskjdmfkld");
+		test("hello");
+	}
+	
+	public static void test(String s) {
+		System.out.println(s);
 	}
 }
